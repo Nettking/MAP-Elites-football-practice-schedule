4bb4c0f01872507656e74403ae9b58cabe9aaf76
Nettking <m@nettking.no>
2023-03-03 16:15:19 +0100

    Update __init__.py

--- MAP_Elites/__init__.py
+++ MAP_Elites/__init__.py
@@ -1,3 +1,4 @@
 from MAP_Elites.import_drills import *
 from MAP_Elites.calculate_player_satisfaction import *
 from MAP_Elites.calculate_fitness import *
+from MAP_Elites.update_elite_archive import *
